66de476e458c85d3dd941cd2f72196e565f30f4c
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2023-04-26 00:19:19 +0000

    Sync LeetCode submission Runtime - 1 ms (98.00%), Memory - 39.7 MB (100.00%)

---
+++ 0258-add-digits/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int addDigits(int num) {
+        if (num<9){
+            return num;
+        }
+        else{
+            if(num%9 != 0){
+                return num%9;    
+            }
+            else{
+                return 9;
+            }
+
+        }
+    }
+}
