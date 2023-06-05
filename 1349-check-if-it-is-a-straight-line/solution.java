ae9aaa6b76a0a51a3f494cba0f218eef553b098c
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2023-06-05 05:32:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.1 MB (100.00%)

---
+++ 1349-check-if-it-is-a-straight-line/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int a = coordinates[1][0] - coordinates[0][0];
+        int b = coordinates[1][1] - coordinates[0][1];
+
+        for(int i = 2; i<coordinates.length; i++){
+            if((b*(coordinates[i][0] - coordinates[0][0])) != (a*(coordinates[i][1] - coordinates[0][1]))){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
