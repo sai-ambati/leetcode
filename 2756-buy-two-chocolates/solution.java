b01f4b708f8248a4f61d708f9f695dad6c555746
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2023-12-21 18:09:55 +0000

    Sync LeetCode submission Runtime - 2 ms (56.52%), Memory - 44.7 MB (99.90%)

--- 2756-buy-two-chocolates/solution.java
+++ 2756-buy-two-chocolates/solution.java
@@ -1,18 +1,13 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        // Sort the Array in Increasing Order
         Arrays.sort(prices);
 
-        // Minimum Cost
-        int minCost = prices[0] + prices[1];
 
-        // We can buy chocolates only if we have enough money
-        if (minCost <= money) {
-            // Return the Amount of Money Left
-            return money - minCost;
-        } else {
-            // We cannot buy chocolates. Return the initial amount of money
+        if(prices[0] + prices[1] > money){
             return money;
+        }
+        else{
+            return money - (prices[0] + prices[1]);
         }
     }
 }
