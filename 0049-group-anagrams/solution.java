f2e55d0bfabd50bb2e0014a744b33c4bb68558e3
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-02-06 14:07:27 +0000

    Sync LeetCode submission Runtime - 8 ms (37.60%), Memory - 47.2 MB (99.99%)

--- 0049-group-anagrams/solution.java
+++ 0049-group-anagrams/solution.java
@@ -1,35 +1,25 @@
+
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> hm = new HashMap<>();
+        Map<String, Integer> mp = new HashMap<>();
+        List<List<String>> ans = new ArrayList<>();
 
-        for(int i = 0; i<strs.length; i++){
-            String tempString = sort(strs[i]);
-
-            if(!hm.containsKey(tempString)){
-                List<String> ls = new ArrayList<>();
-                ls.add(strs[i]);
-                hm.put(tempString, ls);
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortedStr = new String(chars);
+            
+            if (mp.containsKey(sortedStr)) {
+                ans.get(mp.get(sortedStr)).add(str);
+            } else {
+                mp.put(sortedStr, ans.size());
+                ans.add(new ArrayList<>(Arrays.asList(str)));
             }
-
-            else{
-                List<String> ls = hm.get(tempString);
-                ls.add(strs[i]);
-                hm.put(tempString, ls);
-            }
-
-
         }
-
-        List<List<String>> ans= new ArrayList<>();
-
-        hm.forEach((key, value) -> {ans.add(value);});
 
         return ans;
     }
-
-    static String sort(String string){
-        char[] ca = string.toCharArray();
-Arrays.sort(ca);
-        return new String(ca);
-    }
 }
+
+
