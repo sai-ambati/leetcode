6762f0630041b6c4df32f83d3a2d372a01d926f9
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-02-16 11:48:11 +0000

    Sync LeetCode submission Runtime - 45 ms (80.24%), Memory - 63.9 MB (100.00%)

---
+++ 1604-least-number-of-unique-integers-after-k-removals/solution.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+        // Map to track the frequencies of elements
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        // List to track all the frequencies
+        List<Integer> frequencies = new ArrayList<>(map.values());
+
+        // Sorting the frequencies
+        Collections.sort(frequencies);
+
+        // Tracking the number of elements removed
+        int elementsRemoved = 0;
+
+        for (int i = 0; i < frequencies.size(); i++) {
+            // Removing frequencies[i] elements, which equates to
+            // removing one unique element
+            elementsRemoved += frequencies.get(i);
+
+            // If the number of elements removed exceeds k, return
+            // the remaining number of unique elements
+            if (elementsRemoved > k) {
+                return frequencies.size() - i;
+            }
+        }
+
+        // We have removed all elements, so no unique integers remain
+        // Return 0 in this case
+        return 0;
+    }
+}
