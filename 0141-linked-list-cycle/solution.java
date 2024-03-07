83985fb64ecbfcdecc0970b885a4bfdf61132bb4
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-03-07 04:25:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.5 MB (99.98%)

--- 0141-linked-list-cycle/solution.java
+++ 0141-linked-list-cycle/solution.java
@@ -9,18 +9,25 @@
  *     }
  * }
  */
-
-
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-        while (head != null) {
-            if (set.contains(head)) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (slow == fast) {
                 return true;
             }
-            set.add(head);
-            head = head.next;
         }
+
         return false;
     }
 }
+
