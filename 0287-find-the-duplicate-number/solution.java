a18ea2c0f9bfdb33c915d2d45b98b6d5e98887f7
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-03-24 09:42:09 +0000

    Sync LeetCode submission Runtime - 4 ms (90.63%), Memory - 58.3 MB (99.97%)

--- 0287-find-the-duplicate-number/solution.java
+++ 0287-find-the-duplicate-number/solution.java
@@ -1,16 +1,16 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int n = nums.length;
-
-        for(int num:nums){
-            int index = Math.abs(num);
-            if(nums[index] <0){
-                return index;
-            }
-
-            nums[index] = -nums[index];
+        int slow = nums[0];
+        int fast = nums[0];
+        do { 
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        fast = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-
-        return n;
+        return fast;
     }
 }
