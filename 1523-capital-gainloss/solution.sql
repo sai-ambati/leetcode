2857a4b4224cad9e0699facd3cb12fae16780c6d
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-04-13 16:14:19 +0000

    Sync LeetCode submission Runtime - 1230 ms (5.02%), Memory - 0.0B (100.00%)

---
+++ 1523-capital-gainloss/solution.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+select distinct stock_name,
+
+sum(
+    case
+    when operation = 'Buy'
+    then -price
+    else price
+    end
+) over(partition by stock_name) as capital_gain_loss
+
+from Stocks
