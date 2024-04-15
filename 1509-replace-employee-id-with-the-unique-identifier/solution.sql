a69bdedf24c2b565b679cd4fbe5c15e816e6f63f
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-04-15 11:53:40 +0000

    Sync LeetCode submission Runtime - 2525 ms (5.00%), Memory - 0.0B (100.00%)

--- 1509-replace-employee-id-with-the-unique-identifier/solution.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/solution.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-select e2.unique_id, e1.name
-from Employees e1 
-left join EmployeeUNI e2 
-on e1.id = e2.id
+select eu.unique_id, e.name
+from Employees e 
+left join EmployeeUNI eu 
+on e.id = eu.id;
