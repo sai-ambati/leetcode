406ac0b669aaeffa9ab23ee780bca6f543ee5cdd
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-04-15 11:48:23 +0000

    Sync LeetCode submission Runtime - 848 ms (5.02%), Memory - 0.0B (100.00%)

--- 1258-article-views-i/solution.sql
+++ 1258-article-views-i/solution.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
 
-select distinct author_id as id from Views
-where author_id = viewer_id 
+select distinct author_id as id
+from Views 
+where author_id = viewer_id
 order by id
