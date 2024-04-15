fc9d00861bdc22345c9d94a54af7c640e0a3e7fb
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-04-15 13:40:21 +0000

    Sync LeetCode submission Runtime - 1030 ms (5.01%), Memory - 0.0B (100.00%)

--- 0197-rising-temperature/solution.sql
+++ 0197-rising-temperature/solution.sql
@@ -1,12 +1,10 @@
 # Write your MySQL query statement below
 
-with cte as(
+select id
+from(
     select *,
-    lag(recordDate) over(order by recordDate) as prev_date,
-    lag(temperature) over(order by recordDate) as prev_temp
+    lag(recordDate) over(order by recordDate) as prevDay,
+    lag(temperature) over(order by recordDate) as prevDayTemp
     from Weather
-)
-
-select id from cte
-where datediff(recordDate, prev_date) = 1 
-and temperature > prev_temp
+) tbl
+where dateDiff(recordDate, prevDay) = 1 and temperature > prevDayTemp;
