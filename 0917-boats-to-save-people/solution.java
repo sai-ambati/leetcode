c8f4f45a7d3066374f96e2905edd33b8081e24f9
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-05-04 18:11:03 +0000

    Sync LeetCode submission Runtime - 17 ms (98.28%), Memory - 54.9 MB (99.90%)

--- 0917-boats-to-save-people/solution.java
+++ 0917-boats-to-save-people/solution.java
@@ -1,28 +1,15 @@
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
-        if(people.length == 1){
-            return 1;
-        }
-
+        int boats = 0;
         Arrays.sort(people);
-
-        int start = 0;
-        int end = people.length - 1;
-
-        int count = 0;
-        while(start<=end){
-
-            count++;
-            if(people[start] + people[end] <= limit){
-                // count++;
-                start++;
-                end--;
+        int i=0,j=people.length-1;
+        while(i<=j){
+            if((people[j]+people[i])<=limit){
+                i++;
             }
-            else{
-                // count++;
-                end--;
-            }
+            j--;
+            boats++;
         }
-        return count;
+        return boats;
     }
 }
