c87ec6d6372a6a6f20c2ab12c7efec4c048813c3
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-11 16:58:22 +0000

    Sync LeetCode submission Runtime - 1 ms (99.56%), Memory - 41.6 MB (100.00%)

---
+++ 1720-crawler-log-folder/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public int minOperations(String[] logs) {
+        Stack<String> folderStack = new Stack<>();
+
+        for (String currentOperation : logs) {
+            if (currentOperation.equals("../")) {
+                // Move up to parent directory if not already at main folder
+                if (!folderStack.empty()) {
+                    folderStack.pop();
+                }
+            } else if (!currentOperation.equals("./")) {
+                // Enter a new folder
+                folderStack.push(currentOperation);
+            }
+            // Ignore "./" operations as they don't change the current folder
+        }
+
+        // The stack size represents the number of folders deep we are
+        return folderStack.size();
+    }
+}
