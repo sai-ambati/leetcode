e8deefb664fd365d580b2462d2002cb975e4ab63
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-12 13:11:21 +0000

    Sync LeetCode submission Runtime - 1224 ms (42.31%), Memory - 0.0B (100.00%)

--- 1174-sales-analysis-iii/solution.sql
+++ 1174-sales-analysis-iii/solution.sql
@@ -1,20 +1,12 @@
 # Write your MySQL query statement below
 
-with tbl as(
-    select *
-    from Sales 
-    where product_id not in(
-        select product_id from Sales 
-        where sale_date < '2019-01-01' or sale_date> '2019-03-31'
-    )
-)
-
 select distinct p.product_id, p.product_name
-from Product p 
-join tbl s 
-on p.product_id = s.product_id
-
-
-
-
-
+from Product p
+join Sales s
+on p.product_id = s.product_id 
+where sale_date between '2019-01-01' and '2019-03-31'
+and s.product_id not in(
+    select distinct product_id
+    from Sales
+    where sale_date < '2019-01-01' or sale_date > '2019-03-31'
+)
