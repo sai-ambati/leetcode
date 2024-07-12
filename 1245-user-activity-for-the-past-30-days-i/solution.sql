2d5b99cbf80f0255543fbbae183a9174281dedf1
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-12 13:32:04 +0000

    Sync LeetCode submission Runtime - 400 ms (100.00%), Memory - 0.0B (100.00%)

--- 1245-user-activity-for-the-past-30-days-i/solution.sql
+++ 1245-user-activity-for-the-past-30-days-i/solution.sql
@@ -1,14 +1,6 @@
 # Write your MySQL query statement below
-select *
-from(
-select activity_date as day, 
-count(distinct user_id) as active_users
+
+select activity_date as day, count(distinct user_id) as active_users
 from Activity
-group by activity_date
-having datediff('2019-07-27' , activity_date ) < 30
-) tbl
-where day <= '2019-07-27'
--- where activity_date <= '2019-07-27'
--- or datediff(activity_date, '2019-07-27') <30
-
-
+where datediff('2019-07-27', activity_date) < 30 and datediff('2019-07-27', activity_date) >=0
+group by activity_date;
