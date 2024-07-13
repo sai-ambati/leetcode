5a52f399c4c20006b21b4547102c8f53acfa5dd1
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 17:10:43 +0000

    Sync LeetCode submission Runtime - 306 ms (88.70%), Memory - 0.0B (100.00%)

--- 0602-friend-requests-ii-who-has-the-most-friends/solution.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/solution.sql
@@ -1,15 +1,17 @@
 # Write your MySQL query statement below
 
 with cte as(
-    select requester_id as id from RequestAccepted
+    select *
+    from RequestAccepted
 
     union all
 
-    select accepter_id as id from RequestAccepted
+    select accepter_id, requester_id, accept_date
+    from RequestAccepted
 )
 
-select id, count(id) as num
+select requester_id as id, count(accepter_id) as num
 from cte
-group by id
-order by count(id) desc
-limit 1
+group by requester_id
+order by num desc
+limit 1;
