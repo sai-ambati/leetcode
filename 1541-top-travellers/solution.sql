f5418411d4e7359bd10266acc32947c6ed069102
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 02:48:00 +0000

    Sync LeetCode submission Runtime - 885 ms (48.31%), Memory - 0.0B (100.00%)

--- 1541-top-travellers/solution.sql
+++ 1541-top-travellers/solution.sql
@@ -1,16 +1,11 @@
 # Write your MySQL query statement below
-select u.name,
-case 
-when tbl.travelled_distance is null then 0
-else tbl.travelled_distance end as travelled_distance
-from users u 
-left join (
-select distinct user_id, sum(distance) over(partition by user_id) as travelled_distance
-from Rides
-) tbl 
 
-on u.id = tbl.user_id
+select u.name, ifnull(sum(r.distance), 0) as travelled_distance
 
-order by travelled_distance desc, u.name
+from Users u
+left join Rides r
+on u.id = r.user_id
 
+group by u.id, u.name
+order by travelled_distance desc, u.name;
 
