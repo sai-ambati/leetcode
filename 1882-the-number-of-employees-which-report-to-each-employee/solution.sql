b0fb0b509b8313f4e50695bc6c36a23cf25aeb75
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 04:40:21 +0000

    Sync LeetCode submission Runtime - 619 ms (91.31%), Memory - 0.0B (100.00%)

--- 1882-the-number-of-employees-which-report-to-each-employee/solution.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/solution.sql
@@ -1,12 +1,8 @@
 # Write your MySQL query statement below
 
-select m.employee_id, m.name, count(*) as reports_count, 
-case
-when ceil(avg(e.age))-avg(e.age) >0.5
-then floor(avg(e.age))
-else ceil(avg(e.age)) end as average_age
-from Employees e
-join Employees m 
-on e.reports_to = m.employee_id
-group by m.employee_id, m.name
-order by m.employee_id
+select t2.employee_id, t2.name, count(*) as reports_count, round(avg(t1.age)) as average_age
+from Employees t1
+join Employees t2
+on t2.employee_id = t1.reports_to
+group by t2.employee_id, t2.name
+order by t2.employee_id;
