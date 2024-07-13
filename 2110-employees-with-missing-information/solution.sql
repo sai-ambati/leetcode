b2fe53f846852ff418f58e84416767b53ec7bdc3
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 05:34:48 +0000

    Sync LeetCode submission Runtime - 506 ms (99.94%), Memory - 0.0B (100.00%)

--- 2110-employees-with-missing-information/solution.sql
+++ 2110-employees-with-missing-information/solution.sql
@@ -1,20 +1,17 @@
 # Write your MySQL query statement below
 
-with cte as(
-    select e.employee_id
-    from Employees e 
-    join Salaries s 
-    on e.employee_id = s.employee_id
+select employee_id
+from Employees
+where employee_id not in(
+    select employee_id from Salaries
 )
 
-select employee_id 
-from Employees
-where employee_id not in(select * from cte)
+union
 
-union all 
-
-select employee_id 
+select employee_id
 from Salaries
-where employee_id not in(select * from cte)
+where employee_id not in(
+    select employee_id from Employees
+)
 
 order by employee_id
