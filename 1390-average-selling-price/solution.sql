7daa51aaf5cca76beb159c433ced200efdf6a4fc
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 02:24:06 +0000

    Sync LeetCode submission Runtime - 636 ms (100.00%), Memory - 0.0B (100.00%)

--- 1390-average-selling-price/solution.sql
+++ 1390-average-selling-price/solution.sql
@@ -1,24 +1,7 @@
 # Write your MySQL query statement below
-with cte as(
-select distinct p.product_id, p.price, u.units, p.start_date, p.end_date, u.purchase_date
-from Prices p 
+
+select p.product_id, ifnull(round(sum(u.units * p.price)/sum(units), 2), 0) as average_price
+from Prices p
 left join UnitsSold u
-on P.product_id = u.product_id
-),
-
-cte2 as(
-    select *
-    from cte
-    where purchase_date between start_date and end_date or purchase_date is null
-)
-
-select cte2.product_id, 
-case 
-when units is null then 0
-else
-round(sum(units*price)/sum(units),2) 
-end as average_price
-from cte2
-group by product_id
--- having purchase_date between start_date and end_date or purchase_date is null
-
+on p.product_id = u.product_id and u.purchase_date between p.start_date and p.end_date
+group by p.product_id;
