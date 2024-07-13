98d9f3986f82d16a7dfe059f6db90db3149192ae
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 17:04:25 +0000

    Sync LeetCode submission Runtime - 524 ms (99.71%), Memory - 0.0B (100.00%)

--- 0585-investments-in-2016/solution.sql
+++ 0585-investments-in-2016/solution.sql
@@ -1,16 +1,25 @@
 # Write your MySQL query statement below
 
+with cte1 as(
+    select distinct i1.pid
+    from Insurance i1
+    join Insurance i2
+    on i1.lat = i2.lat and i1.lon = i2.lon and i1.pid != i2.pid
+),
+
+cte2 as(
+    select distinct i1.pid
+    from Insurance i1
+    join Insurance i2
+    on i1.tiv_2015 = i2.tiv_2015 and i1.pid != i2.pid
+)
+
 select round(sum(tiv_2016), 2) as tiv_2016
 from Insurance
-where tiv_2015 in(
-    select tiv_2015
-    from Insurance
-    group by tiv_2015
-    having count(*) >1
-)
-and (lat, lon) in(
-    select lat, lon
-    from Insurance
-    group by lat, lon
-    having count(*) = 1
+where pid in (
+    select *
+    from cte2
+) and pid not in (
+    select *
+    from cte1
 )
