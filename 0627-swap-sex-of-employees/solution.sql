0d4f0b2a2021d73639cf9e8d01e150086fecb7ff
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 07:48:07 +0000

    Sync LeetCode submission Runtime - 239 ms (94.34%), Memory - 0.0B (100.00%)

--- 0627-swap-sex-of-employees/solution.sql
+++ 0627-swap-sex-of-employees/solution.sql
@@ -1,7 +1,8 @@
 # Write your MySQL query statement below
 
 update salary
-set sex = 
-case sex
-when 'm' then 'f' 
-else 'm' end
+set sex = (case sex
+                when 'm' then 'f'
+                else 'm'
+            end
+)
