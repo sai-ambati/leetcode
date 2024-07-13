3d4fb85e5173c14d73e23ec09207a9876150b816
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 08:50:20 +0000

    Sync LeetCode submission Runtime - 488 ms (36.69%), Memory - 0.0B (100.00%)

--- 1625-group-sold-products-by-the-date/solution.sql
+++ 1625-group-sold-products-by-the-date/solution.sql
@@ -3,9 +3,9 @@
 select sell_date, count(distinct product) as num_sold,
 group_concat(
     distinct product
-    order by product
+    order by product 
     separator ','
 ) as products
-from Activities 
+from Activities
 group by sell_date
-order by sell_date
+order by sell_date asc;
