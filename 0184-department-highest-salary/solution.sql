5f0f1d1930b64758f19259d8d7c73dd0adf08c4d
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 15:23:32 +0000

    Sync LeetCode submission Runtime - 591 ms (99.88%), Memory - 0.0B (100.00%)

--- 0184-department-highest-salary/solution.sql
+++ 0184-department-highest-salary/solution.sql
@@ -1,13 +1,12 @@
 # Write your MySQL query statement below
+
 with cte as(
-    select *,
-    dense_rank() over(partition by departmentId order by salary desc) as den_rank
-    from Employee
+    select d.name as Department, e.name as Employee, e.salary as salary,
+    max(salary) over(partition by departmentId) as max_salary
+    from Employee e
+    join Department d
+    on e.departmentId = d.id
 )
-
-select d.name as Department, c.name as Employee, c.salary as Salary
-from Department d 
-join cte c 
-on d.id = c.departmentId
-where c.den_rank = 1 
-
+select Department, Employee, salary
+from cte
+where salary = max_salary;
