2db7482dd63fa67f8164bc6224c08b892092ac2c
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 05:24:57 +0000

    Sync LeetCode submission Runtime - 477 ms (99.97%), Memory - 0.0B (100.00%)

--- 1948-rearrange-products-table/solution.sql
+++ 1948-rearrange-products-table/solution.sql
@@ -1,33 +1,25 @@
 # Write your MySQL query statement below
-with cte1 as(
-    select product_id, 
-    'store1' as store,
-    store1 as price
-    from Products
-),
 
-cte2 as(
-    select product_id, 
-    'store2' as store,
-    store2 as price
-    from Products
-),
+select product_id, 
+'store1' as store,
+store1 as price
+from Products
+where store1 is not null
 
-cte3 as(
-    select product_id, 
-    'store3' as store,
-    store3 as price
-    from Products
-),
+union all
 
-cte4 as(
-    select * from cte1
-    union all
-    select * from cte2
-    union all 
-    select * from cte3
-)
-select * from cte4
-where price is not null
+select product_id, 
+'store2' as store,
+store2 as price
+from Products
+where store2 is not null
 
+union all
 
+select product_id, 
+'store3' as store,
+store3 as price
+from Products
+where store3 is not null
+
+-- where store1 is not null and store2 is not null and store3 is not null
