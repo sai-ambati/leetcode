356c281855eba8a603ed9ba4a54bcfe9607f1e62
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-13 15:08:05 +0000

    Sync LeetCode submission Runtime - 272 ms (85.43%), Memory - 0.0B (100.00%)

--- 0176-second-highest-salary/solution.sql
+++ 0176-second-highest-salary/solution.sql
@@ -1,8 +1,12 @@
 # Write your MySQL query statement below
 
-select (
-    select distinct salary
-    from Employee 
-    order by salary desc
-    limit 1,1 
+select
+ifnull(
+    (
+        select distinct salary
+        from Employee
+        order by salary desc
+        limit 1,1
+    ),
+    null
 ) as SecondHighestSalary
