f653ff2301461b08be3fa05cf4bbd48312c32959
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-14 04:49:14 +0000

    Sync LeetCode submission Runtime - 1058 ms (100.00%), Memory - 0.0B (100.00%)

--- 1480-movie-rating/solution.sql
+++ 1480-movie-rating/solution.sql
@@ -1,47 +1,26 @@
 # Write your MySQL query statement below
 
-with cte as(
-    select *, 
-    count(movie_id) over(partition by user_id) as movie_count,
-    date_format(created_at, '%Y-%m') as feb_2020
-    -- avg(rating) over(partition by movie_id) as avg_rating
-    from MovieRating
-),
-
-cte2 as(
-    select distinct user_id, movie_count, max(movie_count) over() as max_count
-    from cte
-),
-
-cte3 as(
-    select distinct movie_id, 
-    avg(rating) over(partition by movie_id) as avg_rating
-    -- round(avg_rating, 2) as avg_rating
-    from cte
-    where feb_2020 = '2020-02'
-),
-
-
-cte4 as(
-    select Users.name as results
-    from cte2
+with user_ as(
+    select MovieRating.user_id, Users.name, count(*) as noRev
+    from MovieRating 
     join Users 
-    on Users.user_id = cte2.user_id
-    where movie_count = max_count
-    order by Users.name
+    on MovieRating.user_id = Users.user_id
+    group by user_id
+    order by noREv desc, Users.name
     limit 1
-), 
+),
 
-cte5 as(
-    select Movies.title as results
-    from Movies
-    join cte3 
-    on Movies.movie_id = cte3.movie_id
-    order by avg_rating desc, Movies.title
+rating as(
+    select MovieRating.movie_id, Movies.title, avg(rating) as rat
+    from MovieRating
+    join Movies
+    on MovieRating.movie_id = Movies.movie_id
+    where date_format(created_at, '%Y-%m') = '2020-02'
+    group by movie_id
+    order by rat desc, Movies.title
     limit 1
 )
-
-select * from cte4
+select name as results from user_
 union all
-select * from cte5
+select title from rating
 
