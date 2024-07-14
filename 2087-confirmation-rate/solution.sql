f6c4494a73df9a689f075cdecb91da2e8d1b4dcf
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-14 02:57:11 +0000

    Sync LeetCode submission Runtime - 698 ms (48.38%), Memory - 0.0B (100.00%)

--- 2087-confirmation-rate/solution.sql
+++ 2087-confirmation-rate/solution.sql
@@ -1,22 +1,8 @@
 # Write your MySQL query statement below
 
-with cte as(
-        select s.user_id, c.action,
-    sum(
-        case
-        when action = 'confirmed'
-        then 1
-        else 0
-        end
-    ) over(partition by s.user_id) as confirmed,
-    count(*) over(partition by s.user_id) as total
-    from Signups s
-    left join Confirmations c
-    on s.user_id = c.user_id
-)
-
-
-select distinct user_id,
-round(confirmed/total, 2) as confirmation_rate
-from cte
-
+select s.user_id,
+round(sum(if(c.action = 'confirmed',1,0))/count(*),2) as confirmation_rate
+from Signups s
+left join Confirmations c
+on c.user_id = s.user_id
+group by s.user_id
