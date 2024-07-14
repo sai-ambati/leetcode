c4e7b750264efd3cb659ef9110d9c644bd64afa4
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-14 02:44:11 +0000

    Sync LeetCode submission Runtime - 335 ms (96.08%), Memory - 0.0B (100.00%)

--- 0626-exchange-seats/solution.sql
+++ 0626-exchange-seats/solution.sql
@@ -1,19 +1,11 @@
 # Write your MySQL query statement below
 
 select 
-    case
-        when id = (
-            select max(id)
-            from seat
-        ) and id%2 = 1
-            then id
-        when id % 2 = 1
-            then id+1
-        else id-1
-    end as id,
-
-    student
-
+case
+    when (id = (select max(id) from Seat)) and (id%2=1) then id
+    when id % 2 = 1 then id+1
+    else id-1
+end as id, 
+student
 from Seat
-order by id
-
+order by id;
