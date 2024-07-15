63501834613a3a09332579885c84d0c996ec0466
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-15 17:20:12 +0000

    Sync LeetCode submission Runtime - 426 ms (21.33%), Memory - 0.0B (100.00%)

--- 0182-duplicate-emails/solution.sql
+++ 0182-duplicate-emails/solution.sql
@@ -1,6 +1,4 @@
-# Write your MySQL query statement below
-
-select email as Email
-from Person 
-group by email
-having count(*) > 1;
+ select email
+ from Person 
+ group by email   
+ having count(email)>1 
