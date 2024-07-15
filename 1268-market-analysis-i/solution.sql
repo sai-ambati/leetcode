3e96f3fcdaa71ebc04cbf78fdb0a34f6487897df
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-15 11:55:44 +0000

    Sync LeetCode submission Runtime - 1361 ms (34.49%), Memory - 0.0B (100.00%)

--- 1268-market-analysis-i/solution.sql
+++ 1268-market-analysis-i/solution.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-# Write your MySQL query statement below
 
-select u.user_id as buyer_id, u.join_date, count(o.order_id) orders_in_2019
-from Users u
-left join Orders o on u.user_id = o.buyer_id and date_format(o.order_date, '%Y') = '2019'
-group by 1
+select user_id as buyer_id, join_date, sum(if(year(order_date)=2019,1,0)) as orders_in_2019
+from Users
+left join Orders
+on user_id = buyer_id
+group by user_id
