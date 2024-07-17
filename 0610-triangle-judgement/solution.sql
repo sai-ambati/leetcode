82bd0afcd7080a31d0b9a6d4c8b73f512f83fca8
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-17 17:57:46 +0000

    Sync LeetCode submission Runtime - 2494 ms (5.01%), Memory - 0.0B (100.00%)

--- 0610-triangle-judgement/solution.sql
+++ 0610-triangle-judgement/solution.sql
@@ -1,5 +1,3 @@
 # Write your MySQL query statement below
-
-select *,
-if(x+y > z and y+z > x and z+x >y, 'Yes', 'No') as triangle
-from Triangle;
+select *, case when x+y>z and y+z>x and  x+z>y then 'Yes' else 'No' end as 'triangle'
+from Triangle 
