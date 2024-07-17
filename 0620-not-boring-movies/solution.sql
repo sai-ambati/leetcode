7ffaa4bce9bd69baa39a7e80c66fd2da9fae3443
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-17 18:30:43 +0000

    Sync LeetCode submission Runtime - 200 ms (100.00%), Memory - 0.0B (100.00%)

--- 0620-not-boring-movies/solution.sql
+++ 0620-not-boring-movies/solution.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-
 select *
-from Cinema
-where mod(id, 2) = 1 and description != 'boring'
-order by rating desc;
+from Cinema 
+where (id)%2!=0 and description not like 'boring'
+order by rating desc
