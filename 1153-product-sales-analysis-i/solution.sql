0312d619ff8c8f8ee5337cab4d4a054f4378afc1
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-17 18:39:16 +0000

    Sync LeetCode submission Runtime - 1440 ms (20.00%), Memory - 0.0B (100.00%)

--- 1153-product-sales-analysis-i/solution.sql
+++ 1153-product-sales-analysis-i/solution.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 
-select p.product_name, s.year, s.price
-from Sales s 
-left join Product p
-on s.product_id = p.product_id;
+select P.product_name,S.year,S.price
+from Sales S left join Product P
+using (product_id )
+
