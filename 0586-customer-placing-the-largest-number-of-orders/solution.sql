604a157e2e6e426f38f4395ff0b2cce311be66be
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-17 16:57:44 +0000

    Sync LeetCode submission Runtime - 462 ms (80.63%), Memory - 0.0B (100.00%)

--- 0586-customer-placing-the-largest-number-of-orders/solution.sql
+++ 0586-customer-placing-the-largest-number-of-orders/solution.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-
-
 select customer_number
-from Orders
+from Orders 
 group by customer_number
-order by count(*) desc
-limit 1;
+order by count(order_number)  desc
+limit 1
