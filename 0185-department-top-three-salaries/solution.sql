2c6b4022c93ea10ccd0f7c49d161240ea48cf436
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-28 09:09:04 +0000

    Sync LeetCode submission Runtime - 915 ms (92.38%), Memory - 0.0B (100.00%)

--- 0185-department-top-three-salaries/solution.sql
+++ 0185-department-top-three-salaries/solution.sql
@@ -1,16 +1,13 @@
 # Write your MySQL query statement below
 
 with cte as(
-    select *,
-    dense_rank() over(partition by departmentId order by salary desc) as den_rank
-    from Employee   
+    select *, 
+    dense_rank() over(partition by departmentId order by salary desc) as dr
+    from Employee
 )
 
-select d.name as Department, c.name as Employee, c.salary 
-
-from cte c 
-join Department d 
-on c.departmentId = d.id
-
-where c.den_rank <=3
-
+select d.name as Department, c.name as Employee, c.salary as Salary
+from Department d
+join cte c
+on d.id = c.departmentId
+where dr <= 3
