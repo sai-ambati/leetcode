da2abf8be134f60b1c66e9ce38ed3ac7a7f26150
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-07-28 09:15:35 +0000

    Sync LeetCode submission Runtime - 304 ms (100.00%), Memory - 0.0B (100.00%)

--- 0601-human-traffic-of-stadium/solution.sql
+++ 0601-human-traffic-of-stadium/solution.sql
@@ -1,19 +1,17 @@
 # Write your MySQL query statement below
+
 with cte as(
-    select *,
-    lag(people) over(order by id) as prev_day,
-    lag(people, 2) over(order by id) as prev_prev_day,
-    lead(people) over(order by id) as next_day,
-    lead(people, 2) over(order by id) as next_next_day
+    select *, id-row_number() over(order by id) as rn
     from Stadium
+    where people >= 100
+),
+
+cte2 as(
+    select *, count(*) over(partition by rn) as freq
+    from cte
 )
 
 select id, visit_date, people
-from cte
-
-where prev_prev_day>=100 and prev_day >=100 and people >=100 or
-prev_day>=100 and people>=100 and next_day>=100 or
-people>=100 and next_day>=100 and next_next_day>=100
-
+from cte2
+where freq>=3
 order by visit_date
-
