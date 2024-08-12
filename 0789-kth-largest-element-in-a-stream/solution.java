b6cc061196604a21033c261d8b70422f7dd0722a
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-08-12 17:58:46 +0000

    Sync LeetCode submission Runtime - 14 ms (100.00%), Memory - 47 MB (100.00%)

--- 0789-kth-largest-element-in-a-stream/solution.java
+++ 0789-kth-largest-element-in-a-stream/solution.java
@@ -1,32 +1,26 @@
 class KthLargest {
-    private static int k;
-    private PriorityQueue<Integer> heap;
-    
+
+    PriorityQueue<Integer> minHeap;
+    int k;
+
     public KthLargest(int k, int[] nums) {
+        minHeap = new PriorityQueue<>();
         this.k = k;
-        heap = new PriorityQueue<>();
-        
-        for (int num: nums) {
-            heap.offer(num);
-        }
-        
-        while (heap.size() > k) {
-            heap.poll();
+
+        for (int num : nums) {
+            add(num);
         }
     }
-    
-    public int add(int val) {
-        heap.offer(val);
-        if (heap.size() > k) {
-            heap.poll();
-        }
 
-        return heap.peek();
+    public int add(int val) {
+        // Add to our minHeap if we haven't processed k elements yet
+        // or if val is greater than the top element (the k-th largest)
+        if (minHeap.size() < k || minHeap.peek() < val) {
+            minHeap.add(val);
+            if (minHeap.size() > k) {
+                minHeap.remove();
+            }
+        }
+        return minHeap.peek();
     }
 }
-
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest obj = new KthLargest(k, nums);
- * int param_1 = obj.add(val);
- */
