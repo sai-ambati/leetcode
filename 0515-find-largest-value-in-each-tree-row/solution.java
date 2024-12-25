db5305fc6402c63ec2f25bb7f467db236f7806fe
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-12-25 17:06:53 +0000

    Sync LeetCode submission Runtime - 5 ms (2.51%), Memory - 45.4 MB (99.96%)

--- 0515-find-largest-value-in-each-tree-row/solution.java
+++ 0515-find-largest-value-in-each-tree-row/solution.java
@@ -12,35 +12,34 @@
  *         this.right = right;
  *     }
  * }
- */
-class Solution {
+ */class Solution {
     public List<Integer> largestValues(TreeNode root) {
         if (root == null) {
             return new ArrayList<Integer>();
         }
         
-        List<Integer> ans = new ArrayList<Integer>();
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+        List<Integer> ans = new ArrayList<>();
+        Stack<Pair<TreeNode, Integer>> stack = new Stack<>();
+        stack.push(new Pair<>(root, 0));
         
-        while (!queue.isEmpty()) {
-            int currentLength = queue.size();
-            int currMax = Integer.MIN_VALUE;
+        while (!stack.isEmpty()) {
+            Pair<TreeNode, Integer> pair = stack.pop();
+            TreeNode node = pair.getKey();
+            int depth = pair.getValue();
             
-            for (int i = 0; i < currentLength; i++) {
-                TreeNode node = queue.remove();
-                currMax = Math.max(currMax, node.val);
-                
-                if (node.left != null) {
-                    queue.add(node.left);
-                }
-                
-                if (node.right != null) {
-                    queue.add(node.right);
-                }
+            if (depth == ans.size()) {
+                ans.add(node.val);
+            } else {
+                ans.set(depth, Math.max(ans.get(depth), node.val));
             }
             
-            ans.add(currMax);
+            if (node.left != null) {
+                stack.push(new Pair<>(node.left, depth + 1));
+            }
+            
+            if (node.right != null) {
+                stack.push(new Pair<>(node.right, depth + 1));
+            }
         }
         
         return ans;
