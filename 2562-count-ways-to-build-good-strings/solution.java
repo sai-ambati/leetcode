20c1030ebd3085ed72aa52ced1b405d60fb468e1
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2024-12-30 18:16:25 +0000

    Sync LeetCode submission Runtime - 10 ms (52.77%), Memory - 47 MB (45.76%)

--- 2562-count-ways-to-build-good-strings/solution.java
+++ 2562-count-ways-to-build-good-strings/solution.java
@@ -1,7 +1,9 @@
 class Solution {
+    // Use dp[i] to record to number of good strings of length i.
     int[] dp;
     int mod = 1_000_000_007;
-
+    
+    // Find the number of good strings of length `end`.
     private int dfs(int end, int zero, int one) {
         if (dp[end] != -1)
             return dp[end];
@@ -21,6 +23,7 @@ class Solution {
         Arrays.fill(dp, -1);
         dp[0] = 1;
         
+        // Add up the number of strings with each valid length [low ~ high].
         int answer = 0;
         for (int end = low; end <= high; ++end) {
             answer += dfs(end, zero, one);
