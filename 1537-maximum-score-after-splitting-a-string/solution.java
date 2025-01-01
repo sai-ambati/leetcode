1ba7a238065a7b6d14080dd015d0328ed4485e18
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-01-01 18:50:49 +0000

    Sync LeetCode submission Runtime - 1 ms (99.64%), Memory - 41.4 MB (100.00%)

--- 1537-maximum-score-after-splitting-a-string/solution.java
+++ 1537-maximum-score-after-splitting-a-string/solution.java
@@ -1,39 +1,21 @@
 class Solution {
     public int maxScore(String s) {
-        
-        int l = s.length();
+        int total_ones = 0;
 
-        int[] zeros = new int[l];
-        int[] ones = new int[l];
+        int n = s.length();
 
-        int count = 0;
-        for(int i = 0; i<l ; i++){
-            
-            if(s.charAt(i) - '0' == 0){
-                count++;
-            }
-            zeros[i] = count;
+        for(int i = 0; i<n; i++){
+            total_ones += (s.charAt(i) - '0');
         }
 
-        count = 0;
-        for(int i = l - 1; i>=0; i--){
-            
-            if(s.charAt(i) - '1' == 0){
-                count++;
-            }
-
-            ones[i] = count;
+        int zeros = 0;
+        int ones = 0;
+        int ans = 0;
+        for(int i = 0; i<n-1; i++){
+            zeros += (1-(s.charAt(i) - '0'));
+            ones += (s.charAt(i) - '0');
+            ans = Math.max(ans, zeros + total_ones - ones);
         }
-
-        count = 0;
-
-        for(int i = 0; i<l-1; i++){
-            if(zeros[i] + ones[i+1] > count){
-                count = zeros[i] + ones[i+1];
-            }
-        }
-
-        return count;
-
+        return ans;
     }
 }
