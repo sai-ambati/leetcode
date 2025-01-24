0c00db84d9cbe00a4002ffc68fa16018062c7f0d
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-01-24 21:09:34 +0000

    Sync LeetCode submission Runtime - 4 ms (99.77%), Memory - 55.8 MB (99.79%)

--- 0820-find-eventual-safe-states/solution.java
+++ 0820-find-eventual-safe-states/solution.java
@@ -1,38 +1,38 @@
 class Solution {
-    public boolean dfs(int node, List<List<Integer>> adj, boolean[] visit, boolean[] inStack) {
+
+    public boolean dfs(
+        int node,
+        int[][] adj,
+        boolean[] visit,
+        boolean[] inStack
+    ) {
+        // If the node is already in the stack, we have a cycle.
         if (inStack[node]) {
             return true;
         }
         if (visit[node]) {
             return false;
         }
+        // Mark the current node as visited and part of current recursion stack.
         visit[node] = true;
         inStack[node] = true;
-        for (int neighbor : adj.get(node)) {
+        for (int neighbor : adj[node]) {
             if (dfs(neighbor, adj, visit, inStack)) {
                 return true;
             }
         }
+        // Remove the node from the stack.
         inStack[node] = false;
         return false;
     }
 
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n = graph.length;
-        List<List<Integer>> adj = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
-            for (int node : graph[i]) {
-                adj.get(i).add(node);
-            }
-        }
-
         boolean[] visit = new boolean[n];
         boolean[] inStack = new boolean[n];
 
         for (int i = 0; i < n; i++) {
-            dfs(i, adj, visit, inStack);
+            dfs(i, graph, visit, inStack);
         }
 
         List<Integer> safeNodes = new ArrayList<>();
@@ -41,6 +41,7 @@ class Solution {
                 safeNodes.add(i);
             }
         }
+
         return safeNodes;
     }
 }
