94192363c37018b734c5182c4dd228ed700173cd
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-02-18 01:27:12 +0000

    Sync LeetCode submission Runtime - 1 ms (99.98%), Memory - 46.3 MB (98.59%)

--- 0136-single-number/solution.java
+++ 0136-single-number/solution.java
@@ -1,11 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int ans = 0;
-        int n = nums.length;
-        for(int i = 0; i<n; i++){
-            ans = ans ^ nums[i];
+        for(int x:nums){
+            ans = ans ^ x;
         }
-
         return ans;
     }
 }
