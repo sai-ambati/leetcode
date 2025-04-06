ad386423c9eab1517dd69602f8fb83c9907b3665
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-04-06 16:31:37 +0000

    Sync LeetCode submission Runtime - 13 ms (99.97%), Memory - 43.1 MB (99.97%)

--- 0368-largest-divisible-subset/solution.java
+++ 0368-largest-divisible-subset/solution.java
@@ -1,34 +1,30 @@
 class Solution {
     public List<Integer> largestDivisibleSubset(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        Arrays.sort(nums);
-        
-        int maxSize = 1, maxIndex = 0;
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] % nums[j] == 0) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                    if (dp[i] > maxSize) {
-                        maxSize = dp[i];
-                        maxIndex = i;
-                    }
-                }
+       int n = nums.length;
+       Arrays.sort(nums);
+       int[] count = new int[n];
+       int[] lastIndx = new int[n];
+       Arrays.fill(count,1);
+       Arrays.fill(lastIndx,-1);
+       int maxL = 1, maxI = 0;
+       List<Integer> answer = new ArrayList<>();
+       for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(nums[i]%nums[j] == 0 && count[j]+1 > count[i]){
+                count[i] = count[j]+1;
+                lastIndx[i] = j;
             }
+          }
+        if(maxL < count[i]){
+            maxL = count[i];
+            maxI = i;
         }
-        
-        List<Integer> result = new ArrayList<>();
-        int num = nums[maxIndex];
-        for (int i = maxIndex; i >= 0; i--) {
-            if (num % nums[i] == 0 && dp[i] == maxSize) {
-                result.add(nums[i]);
-                num = nums[i];
-                maxSize--;
-            }
-        }
-        
-        return result;
+       }
+
+       while(maxI != -1){
+        answer.add(nums[maxI]);
+        maxI =  lastIndx[maxI];
+       }
+       return answer; 
     }
 }
-
