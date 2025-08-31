b1e390f420990d8bd90c25acf6db7302ce39d9a5
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-08-31 05:17:52 +0000

    Sync LeetCode submission Runtime - 1 ms (99.98%), Memory - 46.3 MB (98.59%)

--- 0136-single-number/solution.java
+++ 0136-single-number/solution.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int ans = 0;
         for(int x:nums){
-            ans = ans ^ x;
+            ans = ans^x;
         }
         return ans;
     }
