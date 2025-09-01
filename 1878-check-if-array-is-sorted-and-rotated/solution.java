e9c932d8f08fffcfe026b0301262acff1f5f29ec
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-09-01 13:35:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.6 MB (99.95%)

--- 1878-check-if-array-is-sorted-and-rotated/solution.java
+++ 1878-check-if-array-is-sorted-and-rotated/solution.java
@@ -1,23 +1,15 @@
 class Solution {
-
     public boolean check(int[] nums) {
         int n = nums.length;
-        if (n <= 1) return true;
-
-        int inversionCount = 0;
-
-        // For every pair, count the number of inversions.
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] < nums[i - 1]) {
-                ++inversionCount;
+        int cnt = 0;
+        for(int i = 0; i<n-1; i++){
+            if(nums[i] > nums[i+1]){
+                cnt++;
             }
         }
-
-        // Also check between the last and the first element due to rotation
-        if (nums[0] < nums[n - 1]) {
-            ++inversionCount;
+        if(nums[0] < nums[n-1]){
+            cnt++;
         }
-
-        return inversionCount <= 1;
+        return cnt<=1;
     }
 }
