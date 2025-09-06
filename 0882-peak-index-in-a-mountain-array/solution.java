87413a94030e2c13c67ab572de2f3ed5102d7329
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-09-06 05:28:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 56.5 MB (99.81%)

--- 0882-peak-index-in-a-mountain-array/solution.java
+++ 0882-peak-index-in-a-mountain-array/solution.java
@@ -1,13 +1,28 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int l = 0, r = arr.length - 1, mid;
-        while (l < r) {
-            mid = (l + r) / 2;
-            if (arr[mid] < arr[mid + 1])
-                l = mid + 1;
-            else
-                r = mid;
+        int n = arr.length;
+
+        int start = 0;
+        int end = n-1;
+
+        if(arr[1] > arr[0] && arr[1] > arr[2]){
+            return 1;
         }
-        return l;
+        if(arr[n-2] > arr[n-1] && arr[n-2] > arr[n-3]){
+            return n-2;
+        }
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid] > arr[mid+1]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return -1;
     }
 }
