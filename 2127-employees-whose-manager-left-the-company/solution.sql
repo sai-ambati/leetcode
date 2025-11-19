dde2bfa4d870f0b9c16498a3cc79968376139fe3
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-11-19 18:42:29 +0000

    Sync LeetCode submission Runtime - 387 ms (21.43%), Memory - 0.0B (100.00%)

--- 2127-employees-whose-manager-left-the-company/solution.sql
+++ 2127-employees-whose-manager-left-the-company/solution.sql
@@ -1,9 +1,5 @@
 # Write your MySQL query statement below
-
 select employee_id
 from Employees
-where manager_id not in (
-    select employee_id
-    from Employees
-) and salary < 30000
-order by employee_id;
+where salary < 30000 and manager_id not in (select employee_id from Employees)
+order by employee_id
