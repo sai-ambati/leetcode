b5eabef515800be904a37b424ff735bed7e8c00f
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-11-19 15:05:00 +0000

    Sync LeetCode submission Runtime - 363 ms (96.64%), Memory - 0.0B (100.00%)

--- 0181-employees-earning-more-than-their-managers/solution.sql
+++ 0181-employees-earning-more-than-their-managers/solution.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 
-select E.name as Employee from  
-Employee E left join Employee M 
-on E.managerId =M.id 
-where E.salary > M.salary
-
+select e.name as Employee
+from Employee e 
+left join Employee m 
+on e.managerId = m.id
+where e.salary > m.salary
