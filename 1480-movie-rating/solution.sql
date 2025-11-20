f346d1d4a355b92d7936d022eef93077b5a8f493
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-11-20 13:07:21 +0000

    Sync LeetCode submission Runtime - 1314 ms (70.77%), Memory - 0.0B (100.00%)

--- 1480-movie-rating/solution.sql
+++ 1480-movie-rating/solution.sql
@@ -1,26 +1,21 @@
 # Write your MySQL query statement below
-
-with user_ as(
-    select MovieRating.user_id, Users.name, count(*) as noRev
-    from MovieRating 
-    join Users 
-    on MovieRating.user_id = Users.user_id
-    group by user_id
-    order by noREv desc, Users.name
-    limit 1
-),
-
-rating as(
-    select MovieRating.movie_id, Movies.title, avg(rating) as rat
-    from MovieRating
-    join Movies
-    on MovieRating.movie_id = Movies.movie_id
-    where date_format(created_at, '%Y-%m') = '2020-02'
-    group by movie_id
-    order by rat desc, Movies.title
+(
+    select u.name as results
+    from Users u
+    join MovieRating m
+    on u.user_id = m.user_id
+    group by m.user_id, u.name
+    order by count(*) desc, u.name
     limit 1
 )
-select name as results from user_
 union all
-select title from rating
-
+(
+    select m.title as results
+    from Movies m
+    join MovieRating mr
+    on m.movie_id = mr.movie_id
+    where date_format(created_at, '%Y-%m') = '2020-02'
+    group by mr.movie_id
+    order by avg(rating) desc, m.title
+    limit 1
+);
