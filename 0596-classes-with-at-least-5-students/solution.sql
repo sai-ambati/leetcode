7346099d7a5367d0486f7f584a07eda3da077771
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2025-11-20 12:05:13 +0000

    Sync LeetCode submission Runtime - 313 ms (96.23%), Memory - 0.0B (100.00%)

--- 0596-classes-with-at-least-5-students/solution.sql
+++ 0596-classes-with-at-least-5-students/solution.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select  class
-from Courses 
-group by class    
-having count(student) >=5;
+select class
+from Courses
+group by class
+having count(*) >= 5
