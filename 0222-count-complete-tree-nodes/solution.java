c324849821a40b023bbd2b82f950963383060fce
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-02-08 13:41:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 49.8 MB (12.83%)

---
+++ 0222-count-complete-tree-nodes/solution.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        // base case
+        if(root == null){
+            return 0;
+        }
+
+        // recursive case
+        int leftCount = countNodes(root.left);
+        int rightCount = countNodes(root.right);
+
+        return leftCount + rightCount + 1;
+    }
+}
