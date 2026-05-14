19d8c11bc30f3b7f5ddfa9bf8b7996ab05a356de
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-05-14 16:06:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.7 MB (21.83%)

--- 1580-shuffle-the-array/solution.java
+++ 1580-shuffle-the-array/solution.java
@@ -2,12 +2,11 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
         int[] ans = new int[2*n];
 
-        for(int i=0,j=0; i<n;){
-            ans[j] = nums[i];
-            ans[j+1] = nums[i+n];
-
-            i++;j=j+2;
+        for(int i = 0; i<2*n; i+=2){
+            ans[i] = nums[i/2];
+            ans[i+1] = nums[i/2+n];
         }
+
         return ans;
     }
 }
