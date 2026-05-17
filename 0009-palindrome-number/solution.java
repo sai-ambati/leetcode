4e7480f205c3aceaeff16179b787749d82b435e3
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-05-17 15:48:45 +0000

    Sync LeetCode submission Runtime - 5 ms (79.36%), Memory - 45.8 MB (72.04%)

--- 0009-palindrome-number/solution.java
+++ 0009-palindrome-number/solution.java
@@ -1,25 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-
         if(x<0){
             return false;
         }
-
-        int temp = x;
         int rev = 0;
-        int ld;
-        while(temp>0){
-            ld = temp%10;
-            rev = rev *10 +ld;
-            temp = temp/10;
+        int temp = x;
+        while(temp != 0){
+            rev = rev*10 + temp%10;
+            temp /= 10;
         }
-
-        if(rev == x){
-            return true;
-        }
-        else{
-            return false;
-        }
-        
+        return rev == x;
     }
 }
