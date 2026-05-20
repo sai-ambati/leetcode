fe3c86b39de0d0276cd7049e1200485fa8c642bc
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-05-20 01:50:39 +0000

    Sync LeetCode submission Runtime - 6 ms (20.35%), Memory - 47.8 MB (5.37%)

--- 2766-find-the-prefix-common-array-of-two-arrays/solution.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/solution.java
@@ -1,27 +1,29 @@
 class Solution {
-
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
         int n = A.length;
-        int[] prefixCommonArray = new int[n];
-        int[] frequency = new int[n + 1];
-        int commonCount = 0;
-
-        // Iterate through the elements of both arrays
-        for (int currentIndex = 0; currentIndex < n; ++currentIndex) {
-            // Increment frequency of current elements in A and B
-            // Check if the element in A has appeared before (common in prefix)
-            frequency[A[currentIndex]] += 1;
-            if (frequency[A[currentIndex]] == 2) ++commonCount;
-
-            // Check if the element in B has appeared before (common in prefix)
-            frequency[B[currentIndex]] += 1;
-            if (frequency[B[currentIndex]] == 2) ++commonCount;
-
-            // Store the count of common elements for the current prefix
-            prefixCommonArray[currentIndex] = commonCount;
+        for(int i=1; i<=n; i++){
+            hm.put(i, 2);
         }
+        int[] ans = new int[n];
 
-        // Return the final array with counts of common elements in each prefix
-        return prefixCommonArray;
+        for(int i = 0; i<n; i++){
+
+            hm.put(A[i], hm.get(A[i])-1);
+            hm.put(B[i], hm.get(B[i])-1);
+            if(A[i] == B[i]){
+                hm.remove(A[i]);
+            }
+            else{
+                if(hm.get(A[i]) == 0){
+                    hm.remove(A[i]);
+                }
+                if(hm.get(B[i]) == 0){
+                    hm.remove(B[i]);
+                }
+            }
+            ans[i] = n-hm.size();
+        }
+        return ans;
     }
 }
