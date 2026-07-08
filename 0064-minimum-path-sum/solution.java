ba858fb2471547081c67c4fa18a8f8cd7c2f660f
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-07-08 14:02:08 +0000

    Sync LeetCode submission Runtime - 6 ms (10.03%), Memory - 50.4 MB (21.61%)

--- 0064-minimum-path-sum/solution.java
+++ 0064-minimum-path-sum/solution.java
@@ -1,31 +1,27 @@
 class Solution {
     public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
 
-        // for(int i=1; i<grid[0].length; i++){
-        //     grid[0][i] += grid[0][i-1];
-        // }
-        
-        // for(int i=1; i<grid.length; i++){
-        //     grid[i][0] += grid[i-1][0];
-        // }        
-        for(int i=0; i<grid.length;i++){
-            for(int j=0; j<grid[0].length; j++){
+        int[][] dp = new int[m][n];
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
                 if(i==0 && j==0){
-                    grid[i][j] = grid[i][j];
+                    dp[i][j] = grid[i][j];
                 }
                 else if(i==0){
-                    grid[i][j] += grid[i][j-1];
+                    dp[i][j] = dp[i][j-1] + grid[i][j];
                 }
                 else if(j==0){
-                    grid[i][j] += grid[i-1][j];
+                    dp[i][j] = dp[i-1][j] + grid[i][j];
                 }
                 else{
-                    grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+                    dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
                 }
-
             }
         }
 
-        return grid[grid.length-1][grid[0].length-1];
+        return dp[m-1][n-1];
     }
 }
