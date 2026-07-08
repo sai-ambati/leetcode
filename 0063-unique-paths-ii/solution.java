e07508e6c2e274497b5cf084d444f39266c433c4
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-07-08 13:56:38 +0000

    Sync LeetCode submission Runtime - 1 ms (17.13%), Memory - 43.7 MB (11.06%)

--- 0063-unique-paths-ii/solution.java
+++ 0063-unique-paths-ii/solution.java
@@ -1,24 +1,55 @@
-public class Solution {
+class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0 || obstacleGrid[0][0] == 1) {
-            return 0;
-        }
-
+        if(obstacleGrid[0][0] == 1) return 0;
+    
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
 
-        int[] previous = new int[n];
-        int[] current = new int[n];
-        previous[0] = 1;
+        if(obstacleGrid[m-1][n-1] == 1) return 0;
 
-        for (int i = 0; i < m; i++) {
-            current[0] = obstacleGrid[i][0] == 1 ? 0 : previous[0];
-            for (int j = 1; j < n; j++) {
-                current[j] = obstacleGrid[i][j] == 1 ? 0 : current[j-1] + previous[j];
+        if(m==1 && n==1 && obstacleGrid[0][0] == 0) return 1;
+
+        int[][] dp = new int[m][n];
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(obstacleGrid[i][j] == 1){
+                    obstacleGrid[i][j] = -1;
+                    continue;
+                }
+                if(i==0 && j==0){
+                    obstacleGrid[i][j] = 0;
+                }
+                else if(i==0){
+                    if(obstacleGrid[i][j-1] != -1){
+                        obstacleGrid[i][j] = 1;
+                    }
+                    else{
+                        obstacleGrid[i][j] = -1;
+                    }
+                }
+                else if(j==0){
+                    if(obstacleGrid[i-1][j] != -1){
+                        obstacleGrid[i][j] = 1;
+                    }
+                    else{
+                        obstacleGrid[i][j] = -1;
+                    }
+                }
+
+                else{
+                    if(obstacleGrid[i-1][j] != -1 && obstacleGrid[i][j-1] != -1){
+                        obstacleGrid[i][j] = obstacleGrid[i][j-1] + obstacleGrid[i-1][j];
+                    }
+                    else if(obstacleGrid[i-1][j] == -1){
+                        obstacleGrid[i][j] = obstacleGrid[i][j-1];
+                    }
+                    else{
+                        obstacleGrid[i][j] = obstacleGrid[i-1][j];
+                    }
+                }
             }
-            System.arraycopy(current, 0, previous, 0, n);
         }
-
-        return previous[n-1];
+        return obstacleGrid[m-1][n-1] == -1 ? 0: obstacleGrid[m-1][n-1];
     }
 }
