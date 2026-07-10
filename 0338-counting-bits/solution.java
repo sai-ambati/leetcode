161cd85d53d06b511104e1a473bc5046d5179bd7
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-07-10 07:26:34 +0000

    Sync LeetCode submission Runtime - 2 ms (95.87%), Memory - 48.4 MB (86.77%)

--- 0338-counting-bits/solution.java
+++ 0338-counting-bits/solution.java
@@ -1,25 +1,11 @@
 class Solution {
     public int[] countBits(int n) {
-        
-        int[] ans = new int[n+1];
+        int[] dp = new int[n+1];
 
-        for(int i = 0; i<n+1; i++){
-            ans[i] = numberOfSetBits(i);
+        for(int i = 1; i<=n; i++){
+            dp[i] = dp[i>>1] + (i&1);
         }
 
-        return ans;
-    }
-
-    public static int numberOfSetBits(int n){
-        int count = 0;
-
-        int i = 0;
-        while(n>0){
-            count += (n&(1)) >0 ? 1:0;
-            // i++;
-            n=n>>1;
-        }
-
-        return count;
+        return dp;
     }
 }
