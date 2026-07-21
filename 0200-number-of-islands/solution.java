c724001a567cb2cf1134782df5eeef0c0c5e89dd
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-07-21 13:02:27 +0000

    Sync LeetCode submission Runtime - 4 ms (46.13%), Memory - 52.2 MB (68.10%)

--- 0200-number-of-islands/solution.java
+++ 0200-number-of-islands/solution.java
@@ -1,31 +1,31 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) {
-            return 0;
-        }
-        
-        int numIslands = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    numIslands++;
-                    dfs(grid, i, j);
+        int m = grid.length;
+        int n = grid[0].length;
+        boolean[][] visited = new boolean[m][n];
+        int count = 0;
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(grid[i][j] == '1' && !visited[i][j]){
+                    dfsHelper(grid, visited, i, j, m, n);
+                    count++;
                 }
             }
         }
-        
-        return numIslands;
+
+        return count;
     }
-    
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
+
+    public static void dfsHelper(char[][] grid, boolean[][] visited, int i, int j, int m, int n){
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j] == '0' || visited[i][j]){
             return;
         }
+
+        visited[i][j] = true;
         
-        grid[i][j] = '0'; // mark as visited
-        dfs(grid, i + 1, j); // down
-        dfs(grid, i - 1, j); // up
-        dfs(grid, i, j + 1); // right
-        dfs(grid, i, j - 1); // left
+        dfsHelper(grid, visited, i+1, j, m, n);
+        dfsHelper(grid, visited, i-1, j, m, n);
+        dfsHelper(grid, visited, i, j+1, m, n);
+        dfsHelper(grid, visited, i, j-1, m, n);
     }
 }
