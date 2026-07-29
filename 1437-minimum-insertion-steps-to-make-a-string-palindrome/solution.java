c3b424e55c899d24e520dde79fc90b67dc925379
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-07-29 14:04:24 +0000

    Sync LeetCode submission Runtime - 18 ms (90.59%), Memory - 46.8 MB (73.75%)

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/solution.java
@@ -1,29 +1,27 @@
 class Solution {
-    private int lcs(String s1, String s2, int m, int n) {
-        int[] dp = new int[n + 1];
-        int[] dpPrev = new int[n + 1];
-
-        for (int i = 0; i <= m; i++) {
-            for (int j = 0; j <= n; j++) {
-                if (i == 0 || j == 0) {
-                    // One of the two strings is empty.
-                    dp[j] = 0;
-                } else if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
-                    dp[j] = 1 + dpPrev[j - 1];
-                } else {
-                    dp[j] = Math.max(dpPrev[j], dp[j - 1]);
-                }
-            }
-            dpPrev = dp.clone();
-        }
-
-        return dp[n];
-    }
-
     public int minInsertions(String s) {
         int n = s.length();
-        String sReverse = new StringBuilder(s).reverse().toString();
+        int start = 0;
+        int end = n-1;
+        int[][] dp = new int[n][n];
+        for(int i = 0; i<n; i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(s, start, end, dp);
+    }
 
-        return n - lcs(s, sReverse, n, n);
+    public static int solve(String s, int start, int end, int[][] dp){
+        if(start >= end) return 0;
+        
+        if(dp[start][end] != -1){
+            return dp[start][end];
+        }
+        if(s.charAt(start) == s.charAt(end)){
+            dp[start][end] = solve(s, start+1, end-1, dp);
+            return dp[start][end];
+        }
+
+        dp[start][end] = Math.min(solve(s, start+1, end, dp), solve(s, start, end-1, dp)) + 1;
+        return dp[start][end];
     }
 }
