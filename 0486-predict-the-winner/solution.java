8136ab85118bc19a75a75aef2cf88716699393b5
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-08-01 14:34:06 +0000

    Sync LeetCode submission Runtime - 65 ms (17.77%), Memory - 42.9 MB (35.57%)

--- 0486-predict-the-winner/solution.java
+++ 0486-predict-the-winner/solution.java
@@ -1,15 +1,26 @@
 class Solution {
-    public boolean PredictTheWinner(int[] nums) {
-        int n = nums.length;
-        int[] dp = Arrays.copyOf(nums, n);
-
-        for(int diff = 1; diff < n; ++diff){
-            for(int left = 0; left< n - diff; ++left){
-                int right = left + diff;
-                dp[left] = Math.max(nums[left] - dp[left + 1], nums[right] - dp[left]);
-            }
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length-1;
+        if(n==0) return true;
+        int sum = 0;
+        for(int x:nums){
+            sum += x;
         }
+        int p1 = solve(nums, 0, n);
+        int p2 = sum-p1;
+        return p1>=p2;
+    }
 
-        return dp[0] >=0;
+    public static int solve(int[] nums, int i, int j){
+        if(i > j){
+            return 0;
+        }
+        if(i==j) return nums[i];
+
+        int left = Math.min(solve(nums, i+2, j), solve(nums, i+1, j-1)) + nums[i];
+        int right = Math.min(solve(nums, i, j-2), solve(nums, i+1, j-1)) + nums[j];
+
+        return Math.max(left, right);
+        
     }
 }
