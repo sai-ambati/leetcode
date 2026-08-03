6fba6658466ed64c92e97fb44711d4247481a4f7
sai-ambati <169011359+sai-ambati@users.noreply.github.com>
2026-08-03 18:13:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (23.55%)

--- 0213-house-robber-ii/solution.java
+++ 0213-house-robber-ii/solution.java
@@ -1,37 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-
-        if(n==1){
-            return nums[0];
-        }
-
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        int a = solve(0, n-2, nums, dp);
-        Arrays.fill(dp, -1);
-        int b = solve(1, n-1, nums, dp);
+        if(n==1) return nums[0];
+        int[] dp1 = new int[n];
+        Arrays.fill(dp1, -1);
+        int a = helper(nums, 0, n-2,dp1);
+        int[] dp2 = new int[n];
+        Arrays.fill(dp2, -1);
+        int b = helper(nums, 1, n-1, dp2);
         return Math.max(a, b);
     }
 
-    public static int solve(int start, int k, int[] nums, int[] dp){
-        if(k == start){
-            dp[start] =  nums[start];
-            return dp[start];
+    public static int helper(int[] nums, int i, int n, int[] dp){
+        if(i>n) return 0;
+        if(dp[i] != -1){
+            return dp[i];
         }
-
-        if(k<start){
-            return 0;
-        }
-
-        if(dp[k] != -1){
-            return dp[k];
-        }
-
-        int a = solve(start, k-1, nums, dp);
-        int b = solve(start, k-2, nums, dp);
-
-        dp[k] = Math.max(a, b+nums[k]);
-        return dp[k];
+        int inc = helper(nums, i+2, n, dp) + nums[i];
+        int exc = helper(nums, i+1, n, dp);
+        dp[i] = Math.max(inc, exc);
+        return dp[i];
     }
+
 }
